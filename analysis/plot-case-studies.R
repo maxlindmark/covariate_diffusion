3c67177165b5eec0d07dcac6e8ae5b99f25feeb2
Max Lindmark <max.lindmark@slu.se>
2024-08-08 09:47:19 +0200

    minor edits to plots...

--- analysis/plot-case-studies.R
+++ analysis/plot-case-studies.R
@@ -7,17 +7,20 @@ library(patchwork)
 library(ggplot2)
 library(ggsidekick); theme_set(theme_sleek())
 library(viridis)
+library(sf)
+
+root_dir <- here::here(".")
 
 ### Plot results from the case studies
 # Read data
-bering <- read.csv(paste0(here::here(), "/results/2024-08-07_identity_LNP/Results_cz.csv")) |>
+bering <- read.csv(paste0(root_dir, "/results/2024-08-07_identity_LNP/Results_cz.csv")) |>
   mutate("Diffusion\nfavoured" = ifelse(deltaAIC < 0, "N", "Y"),
          abs_delta_aic = ifelse(abs(deltaAIC) > 2, "<2", ">2")) |>
   rename(covar_corr = corr_depth) |>
   mutate(abb_name = X)
 
 # Breeding bird case
-bb <- read.csv(paste0(here::here(), "/results/2024-08-07_LNP/Results_bb_cz.csv")) |>
+bb <- read.csv(paste0(root_dir, "/results/2024-08-07_LNP/Results_bb_cz.csv")) |>
   mutate("Diffusion\nfavoured" = ifelse(deltaAIC < 0, "N", "Y"),
          abs_delta_aic = ifelse(abs(deltaAIC) > 2, "<2", ">2")) |>
   rename(covar_corr = corr_pop_dens) |>
@@ -67,35 +70,41 @@ sub <- bb_stuff |> filter(species %in% c("Sturnus_vulgaris", "Pheucticus_melanoc
 # Main plot?!
 p1 <- mp_bb_s +
   geom_sf(data = sub, aes(fill = orig), color = NA) +
-  labs(fill = NULL,
-       title = "(a)\nlog(population density)") +
+  labs(tag = "(a) log(population density)",
+       fill = NULL) +
   facet_wrap(~NA) +
   guides(fill = guide_colorbar(position = "inside", title.position = "top", title.hjust = 0.5)) +
   theme(legend.position.inside = c(0.16, 0.07),
-        legend.title = element_blank())
+        legend.title = element_blank(),
+        plot.tag.position = c(0.59, 0.76))
 
 p2 <- mp_bb_s +
   geom_sf(data = sub, aes(fill = diffused), color = NA) +
-  theme(axis.text = element_blank(),
-        axis.title = element_blank()) +
-  labs(title = "(b)\nDiffused log(population density)",
+  labs(tag = "(b) Diffused log(population density)",
        fill = NULL) +
   facet_wrap(~factor(abb_name, levels = c("<i>S. vulgaris</i>",
-                                          "<i>P. melanocephalus</i>")), ncol = 1)
+                                          "<i>P. melanocephalus</i>")), ncol = 1) +
+  theme(axis.text = element_blank(),
+        axis.title = element_blank(),
+        plot.tag.position = c(0.48, 1))
 
 p3 <- mp_bb_s +
   geom_sf(data = sub, aes(fill = `log count diffused`), color = NA) +
-  theme(axis.text = element_blank(),
-        axis.title = element_blank()) +
-  labs(title = "(c)\nlog(predicted count)",
+  labs(tag = "(c) log(predicted count)",
        fill = NULL) +
   scale_fill_viridis(option = "magma") +
   facet_wrap(~factor(abb_name, levels = c("<i>S. vulgaris</i>",
-                                          "<i>P. melanocephalus</i>")), ncol = 1)
+                                          "<i>P. melanocephalus</i>")), ncol = 1) +
+  theme(axis.text = element_blank(),
+        axis.title = element_blank(),
+        plot.tag.position = c(0.5, 1))
 
-((p1 / plot_spacer()) | p2 | p3)
+(p1 | p2 | p3) +
+  plot_layout(widths = c(1, 1, 1)) &
+  theme(plot.tag = element_text(size = 11))
 
-ggsave(paste0(here::here(), "/results/figures/bb_maps.pdf"), width = 19, height = 14, unit = "cm")
+ggsave(paste0(here::here(), "/results/figures/bb_maps.pdf"), width = 20, height = 16, unit = "cm")
+
 
 # Supporting info plot
 mp_bb_fc +
@@ -103,7 +112,6 @@ mp_bb_fc +
   labs(fill = "Difference between diffused and original covariate")
 
 ggsave(paste0(here::here(), "/results/figures/supporting/bb_diffused_original.pdf"), width = 22, height = 23, unit = "cm")
-
 
 
 # EBS map plots
@@ -122,9 +130,10 @@ p1 <- mp_ebs_s +
   facet_wrap(~NA) +
   geom_sf(data = sub, aes(fill = orig, color = orig), linewidth = 0.01) +
   labs(fill = NULL,
-       title = "(a)\nlog(depth)") +
+       tag = "(a) log(depth)") +
   theme(legend.position.inside = c(0.16, 0.07),
-        legend.title = element_blank()) +
+        legend.title = element_blank(),
+        plot.tag.position = c(0.59, 0.76)) +
   scale_fill_viridis(option = "mako", na.value = NA, trans = "fourth_root_power",
                      breaks = c(1, 3, 6, 9), direction = -1) +
   scale_color_viridis(option = "mako", na.value = NA, trans = "fourth_root_power",
@@ -133,8 +142,9 @@ p1 <- mp_ebs_s +
 p2 <- mp_ebs_s +
   geom_sf(data = sub, aes(fill = diffused, color = diffused), linewidth = 0.01) +
   theme(axis.text = element_blank(),
-        axis.title = element_blank()) +
-  labs(title = "(b)\nDiffused log(depth)",
+        axis.title = element_blank(),
+        plot.tag.position = c(0.48, 1)) +
+  labs(tag = "(b) Diffused log(depth)",
        fill = NULL) +
   scale_fill_viridis(option = "mako", na.value = NA, trans = "fourth_root_power",
                      breaks = c(1, 3, 6, 9), direction = -1) +
@@ -145,15 +155,18 @@ p3 <- mp_ebs_s +
   geom_sf(data = sub, aes(fill = `log(dens) diffused`, color = `log(dens) diffused`),
          linewidth = 0.01) +
   theme(axis.text = element_blank(),
-        axis.title = element_blank()) +
-  labs(title = "(c)\nlog(predicted density)",
+        axis.title = element_blank(),
+        plot.tag.position = c(0.5, 1)) +
+  labs(tag = "(c) log(predicted density)",
        fill = NULL) +
   scale_fill_viridis(option = "magma", na.value = NA) +
   scale_color_viridis(option = "magma", na.value = NA)
 
-((p1 / plot_spacer()) | p2 | p3)
+(p1 | p2 | p3) +
+  plot_layout(widths = c(1, 1, 1)) &
+  theme(plot.tag = element_text(size = 11))
 
-ggsave(paste0(here::here(), "/results/figures/ebs_maps.pdf"), width = 19, height = 14, unit = "cm")
+ggsave(paste0(here::here(), "/results/figures/ebs_maps.pdf"), width = 20, height = 16, unit = "cm")
 
 
 # Supporting info plot
