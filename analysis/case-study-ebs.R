20c7ea990e2100325f78a3dc75b8c2a597d29b4b
Max Lindmark <max.lindmark@slu.se>
2024-11-15 09:01:20 +0100

    style code a bit

--- analysis/case-study-ebs.R
+++ analysis/case-study-ebs.R
@@ -9,21 +9,22 @@ source(here::here("analysis/prep-ebs-data.R"))
 
 
 # Compile
-setwd( tmb_dir )
-compile( "movement_kernel_ebs.cpp", framework="TMBad" )
-dyn.load( dynlib("movement_kernel_ebs") )
-#dyn.unload( dynlib("movement_kernel") )
+setwd(tmb_dir)
+compile("movement_kernel_ebs.cpp", framework = "TMBad")
+dyn.load(dynlib("movement_kernel_ebs"))
+# dyn.unload( dynlib("movement_kernel") )
 
 # saving stuff
-species_set = colnames(region_data_all)[30:ncol(region_data_all)]
-N_c = colSums( ifelse(region_data_all[,species_set]>0,1,0) )
-species_set = species_set[ N_c > 1000 ]
+species_set <- colnames(region_data_all)[30:ncol(region_data_all)]
+N_c <- colSums(ifelse(region_data_all[, species_set] > 0, 1, 0))
+species_set <- species_set[N_c > 1000]
 
 #
-param_set = c("obj_diffusion", "obj_null", "deltaAIC", "range", "ln_kappa2", "corr_depth")
-Results_cz = array( NA,
-                    dim = c(length(species_set),length(param_set)),
-                    dimnames = list(species_set,param_set) )
+param_set <- c("obj_diffusion", "obj_null", "deltaAIC", "range", "ln_kappa2", "corr_depth")
+Results_cz <- array(NA,
+  dim = c(length(species_set), length(param_set)),
+  dimnames = list(species_set, param_set)
+)
 
 # Store output for plotting
 stuff_gz_list <- list()
@@ -33,130 +34,141 @@ mean_depth_s <- mean(depth_s)
 sd_depth_s <- sd(depth_s)
 
 # Loop
-#cI = match( "a_yfs", species_set )
-#cI = match( c("a_poll", "cap"), species_set )
-for( cI in seq_along(species_set) ){
-  species = species_set[cI]
+# cI = match( "a_yfs", species_set )
+# cI = match( c("a_poll", "cap"), species_set )
+for (cI in seq_along(species_set)) {
+  species <- species_set[cI]
 
   # Build object
-  if(f_depth=="identity"){
-    depthprime_s = (depth_s - mean_depth_s) / sd_depth_s #(depth_s / 100)
-  }else if(f_depth=="log"){
-    depthprime_s = log(depth_s) - 4.5
+  if (f_depth == "identity") {
+    depthprime_s <- (depth_s - mean_depth_s) / sd_depth_s # (depth_s / 100)
+  } else if (f_depth == "log") {
+    depthprime_s <- log(depth_s) - 4.5
   }
-  Data = list( "method" = "diffusion",
-               "dist" = distribution,
-               "c_i" = region_data_all[,species],
-               "A_is" = A_is,
-               "A_gs" = A_gs,
-               "M0" = spde$c0,
-               "M1" = spde$g1,
-               "M2" = spde$g2,
-               "invsqrtM0" = invsqrtM0,
-               "invM0" = invM0,
-               "depth_s" = depthprime_s,
-               "sim_gmrf" = 0L )
-  Params = list( "beta0"=0,
-                 "beta_j" = c(0.1 ,0.1),
-                 "ln_tau"=0,
-                 "ln_kappa"=0,
-                 "omega_s"=rnorm(nrow(spde$c0)),
-                 "ln_kappa2"=exp(-1) )
-  Random = "omega_s"
+  Data <- list(
+    "method" = "diffusion",
+    "dist" = distribution,
+    "c_i" = region_data_all[, species],
+    "A_is" = A_is,
+    "A_gs" = A_gs,
+    "M0" = spde$c0,
+    "M1" = spde$g1,
+    "M2" = spde$g2,
+    "invsqrtM0" = invsqrtM0,
+    "invM0" = invM0,
+    "depth_s" = depthprime_s,
+    "sim_gmrf" = 0L
+  )
+  Params <- list(
+    "beta0" = 0,
+    "beta_j" = c(0.1, 0.1),
+    "ln_tau" = 0,
+    "ln_kappa" = 0,
+    "omega_s" = rnorm(nrow(spde$c0)),
+    "ln_kappa2" = exp(-1)
+  )
+  Random <- "omega_s"
 
   # Special stuff
-  if( Data$dist == "Tweedie" ){
-    Params$ln_phi = log(2)
-    Params$finv_power = 0
+  if (Data$dist == "Tweedie") {
+    Params$ln_phi <- log(2)
+    Params$finv_power <- 0
   }
-  if( Data$dist == "LNP" ){
-    Params$ln_sigma_eta = log(0.1)
-    Params$eta_i = rnorm(nrow(region_data_all))
-    Random = c(Random, "eta_i")
+  if (Data$dist == "LNP") {
+    Params$ln_sigma_eta <- log(0.1)
+    Params$eta_i <- rnorm(nrow(region_data_all))
+    Random <- c(Random, "eta_i")
   }
 
   # profile = c("beta0","beta_j") is unstable for j_berfl and depthprime_s = (depth_s / 100)
-  Obj = MakeADFun( data = Data,
-                   parameters = Params,
-                   #profile = c("beta0","beta_j"),    # ,"ln_kappa2"
-                   random = Random,
-                   checkParameterOrder = TRUE )
+  Obj <- MakeADFun(
+    data = Data,
+    parameters = Params,
+    # profile = c("beta0","beta_j"),    # ,"ln_kappa2"
+    random = Random,
+    checkParameterOrder = TRUE
+  )
   Obj$env$beSilent()
 
-  if( species %in% c("lhdab", "a_yfs", "j_berfl") ){
-
+  if (species %in% c("lhdab", "a_yfs", "j_berfl")) {
     Obj$par["ln_kappa2"] <- 4
-
   }
 
   # Optimize
-  Opt = nlminb( start = Obj$par,
-                obj = Obj$fn,
-                grad = Obj$gr,
-                control = list(eval.max=1e4, iter.max=1e4, trace=1) )
-  Report = Obj$report()
+  Opt <- nlminb(
+    start = Obj$par,
+    obj = Obj$fn,
+    grad = Obj$gr,
+    control = list(eval.max = 1e4, iter.max = 1e4, trace = 1)
+  )
+  Report <- Obj$report()
 
   # Stationary distribution
-  Results_cz[cI,'obj_diffusion'] = Opt$objective
-  Results_cz[cI,'range'] = Report$Range2
-  Results_cz[cI,'ln_kappa2'] = Opt$par['ln_kappa2']
-  Results_cz[cI,'corr_depth'] = cor( Report$depth_g, as.vector(A_gs%*%Data$depth_s) )
+  Results_cz[cI, "obj_diffusion"] <- Opt$objective
+  Results_cz[cI, "range"] <- Report$Range2
+  Results_cz[cI, "ln_kappa2"] <- Opt$par["ln_kappa2"]
+  Results_cz[cI, "corr_depth"] <- cor(Report$depth_g, as.vector(A_gs %*% Data$depth_s))
 
   # Re-run without covariate diffusion
-  Data2 = Data
-  Data2$method = "null"
-  Params2 = Params[setdiff(names(Params),c("ln_tau2","ln_kappa2"))]
-  Obj2 = MakeADFun( data = Data2,
-                    parameters = Params2,
-                    #profile = c("beta0","beta_j"),
-                    #profile = c("beta_j"),
-                    random = Random,
-                    checkParameterOrder = TRUE )
+  Data2 <- Data
+  Data2$method <- "null"
+  Params2 <- Params[setdiff(names(Params), c("ln_tau2", "ln_kappa2"))]
+  Obj2 <- MakeADFun(
+    data = Data2,
+    parameters = Params2,
+    # profile = c("beta0","beta_j"),
+    # profile = c("beta_j"),
+    random = Random,
+    checkParameterOrder = TRUE
+  )
   Obj2$env$beSilent()
 
   # Optimize
-  Opt2 = nlminb( start = Obj2$par,
-                 obj = Obj2$fn,
-                 grad = Obj2$gr,
-                 control = list(eval.max=1e4, iter.max=1e4, trace=1) )
-  Report2 = Obj2$report()
+  Opt2 <- nlminb(
+    start = Obj2$par,
+    obj = Obj2$fn,
+    grad = Obj2$gr,
+    control = list(eval.max = 1e4, iter.max = 1e4, trace = 1)
+  )
+  Report2 <- Obj2$report()
 
   #
-  Results_cz[cI,'deltaAIC'] = (2*Opt2$objective+2*length(Opt2$par)) - (2*Opt$objective+2*length(Opt$par))
-  Results_cz[cI,'obj_null'] = Opt2$objective
-  write.csv( Results_cz, file=file.path(date_dir,"Results_cz.csv") )
+  Results_cz[cI, "deltaAIC"] <- (2 * Opt2$objective + 2 * length(Opt2$par)) - (2 * Opt$objective + 2 * length(Opt$par))
+  Results_cz[cI, "obj_null"] <- Opt2$objective
+  write.csv(Results_cz, file = file.path(date_dir, "Results_cz.csv"))
 
   # Plot results
-  trunc_dens = function(logdens, ratio=0.01) ifelse( logdens<(max(logdens,na.rm=TRUE)+log(ratio)), NA, logdens )
-  png( file=file.path(date_dir,paste0(species,".png")), width=6, height=6, res=200, units="in" )
-    stuff_gz = st_sf( sf_grid,
-                       "orig"=as.vector(Data$A_gs%*%Data$depth_s),
-                       "log(dens) orig" = trunc_dens(log(Report2$mu_g)),
-                       "pdepth orig" = trunc_dens(Report2$pdepth_g),
-                       "omega orig" = Report2$omega_g,
-
-                       "diffused"=Report$depth_g,
-                       "log(dens) diffused" = trunc_dens(log(Report$mu_g)),
-                       "pdepth diffused" = trunc_dens(Report$pdepth_g),
-                       "omega diffused" = Report$omega_g )
-    #plot(stuff_gz, border=NA )
-    par( mfcol=c(4,2) )
-    for( i in 1:8 ){
-      plot( stuff_gz[i], border=NA, key.pos=NULL, reset=FALSE )
-      plot( sf_usa, add=TRUE, col="grey" )
-      add_legend( round(range(stuff_gz[[i]],na.rm=TRUE),1) )
-    }
+  trunc_dens <- function(logdens, ratio = 0.01) ifelse(logdens < (max(logdens, na.rm = TRUE) + log(ratio)), NA, logdens)
+  png(file = file.path(date_dir, paste0(species, ".png")), width = 6, height = 6, res = 200, units = "in")
+  stuff_gz <- st_sf(sf_grid,
+    "orig" = as.vector(Data$A_gs %*% Data$depth_s),
+    "log(dens) orig" = trunc_dens(log(Report2$mu_g)),
+    "pdepth orig" = trunc_dens(Report2$pdepth_g),
+    "omega orig" = Report2$omega_g,
+    "diffused" = Report$depth_g,
+    "log(dens) diffused" = trunc_dens(log(Report$mu_g)),
+    "pdepth diffused" = trunc_dens(Report$pdepth_g),
+    "omega diffused" = Report$omega_g
+  )
+  # plot(stuff_gz, border=NA )
+  par(mfcol = c(4, 2))
+  for (i in 1:8) {
+    plot(stuff_gz[i], border = NA, key.pos = NULL, reset = FALSE)
+    plot(sf_usa, add = TRUE, col = "grey")
+    add_legend(round(range(stuff_gz[[i]], na.rm = TRUE), 1))
+  }
   dev.off()
 
   stuff_gz_list[[cI]] <- stuff_gz |>
-    dplyr::mutate(species = species_set[cI]
-                  # TODO: scale back covariate for plotting? How about the diffused covariate?
-                  #,
-                  #orig = (orig * sd_depth_s) + mean_depth_s,
-                  #diffused = (diffused * sd_depth_s) + mean_depth_s
-                  )
+    dplyr::mutate(
+      species = species_set[cI]
+      # Scale back covariate for plotting? How about the diffused covariate?
+      # ,
+      # orig = (orig * sd_depth_s) + mean_depth_s,
+      # diffused = (diffused * sd_depth_s) + mean_depth_s
+    )
 }
 
 stuff_gz_df <- dplyr::bind_rows(stuff_gz_list)
 
-saveRDS( stuff_gz_df, file=file.path(date_dir,"stuff_gz_df.rds") )
+saveRDS(stuff_gz_df, file = file.path(date_dir, "stuff_gz_df.rds"))
--- analysis/theory-checks-figures.R
+++ analysis/theory-checks-figures.R
@@ -1,4 +1,3 @@
-
 # FROM: C:\Users\James.Thorson\Desktop\Work files\Collaborations\2024 -- movement kernel for covariates\Old code\covariate_diffusion_2024-02-05.R
 
 library(fmesher)
@@ -14,17 +13,17 @@ library(patchwork)
 set.seed(194301)
 
 #
-loc = matrix( runif(1000), ncol=2 )
-mesh = fm_mesh_2d( loc )
-spde = fm_fem( mesh )
-invM0 = spde$c0
-diag(invM0) = 1/diag(spde$c0)
+loc <- matrix(runif(1000), ncol = 2)
+mesh <- fm_mesh_2d(loc)
+spde <- fm_fem(mesh)
+invM0 <- spde$c0
+diag(invM0) <- 1 / diag(spde$c0)
 
 #
-sf_mesh = st_union(fm_as_sfc(mesh))
-sf_grid = st_make_grid( sf_mesh, n=100 )
-sf_grid = st_intersection( sf_grid, sf_mesh )
-A_gs = fm_evaluator( mesh, loc=st_coordinates(st_centroid(sf_grid)) )$proj$A
+sf_mesh <- st_union(fm_as_sfc(mesh))
+sf_grid <- st_make_grid(sf_mesh, n = 100)
+sf_grid <- st_intersection(sf_grid, sf_mesh)
+A_gs <- fm_evaluator(mesh, loc = st_coordinates(st_centroid(sf_grid)))$proj$A
 
 # Loop through a couple of kappa values to visualize diffusion
 
@@ -32,41 +31,42 @@ stuff_list <- list()
 sum_list <- list()
 
 # Using IID normal deviates. Hold this constant across kappas
-vec2 = rnorm(mesh$n)
+vec2 <- rnorm(mesh$n)
 
-for(i in c("0", "2.5", "5")){
+for (i in c("0", "2.5", "5")) {
+  ln_kappa <- as.numeric(i)
+  ln_tau <- log(1 / (1 + exp(2 * ln_kappa)))
 
-  ln_kappa = as.numeric(i)
-  ln_tau = log( 1 / (1 + exp(2 * ln_kappa)) )
+  invD <- exp(ln_tau) * invM0 %*% (spde$c0 + exp(2 * ln_kappa) * spde$c0 + spde$g1)
+  D <- solve(invD)
 
-  invD = exp(ln_tau) * invM0 %*% (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g1)
-  D = solve(invD)
-
-  which_mid = which.min( rowSums(scale(mesh$loc[,1:2])^2) )
+  which_mid <- which.min(rowSums(scale(mesh$loc[, 1:2])^2))
 
   # Diffusion from a specific point
-  vec1 = rep(0,mesh$n)
-  vec1[which_mid] = 1
+  vec1 <- rep(0, mesh$n)
+  vec1[which_mid] <- 1
 
   # Using IID normal deviates
-  #vec2 = rnorm(mesh$n)
+  # vec2 = rnorm(mesh$n)
 
-  stuff = st_sf( sf_grid,
-                 "orig"=as.numeric(A_gs%*%vec1),
-                 "orig2"=as.numeric(A_gs%*%vec2),
-                 "proj"=as.numeric(A_gs%*%solve(invD,vec1)),
-                 "proj2"=as.numeric(A_gs%*%solve(invD,vec2)))   # , log(as.numeric(vec2b)))
+  stuff <- st_sf(sf_grid,
+    "orig" = as.numeric(A_gs %*% vec1),
+    "orig2" = as.numeric(A_gs %*% vec2),
+    "proj" = as.numeric(A_gs %*% solve(invD, vec1)),
+    "proj2" = as.numeric(A_gs %*% solve(invD, vec2))
+  ) # , log(as.numeric(vec2b)))
 
-  plot( stuff, cex=2, pch=19, border=NA )
+  plot(stuff, cex = 2, pch = 19, border = NA)
 
   stuff_list[[i]] <- stuff |> mutate(kappa = i)
 
-  sum_list[[i]] <- tibble(kappa = i,
-                          sum_orig = sum( stuff$orig ),
-                          sum_proj = sum( stuff$proj ),
-                          sum_orig2 = sum( stuff$orig2 ),
-                          sum_proj2 = sum( stuff$proj2 ))
-
+  sum_list[[i]] <- tibble(
+    kappa = i,
+    sum_orig = sum(stuff$orig),
+    sum_proj = sum(stuff$proj),
+    sum_orig2 = sum(stuff$orig2),
+    sum_proj2 = sum(stuff$proj2)
+  )
 }
 
 stuff <- bind_rows(stuff_list)
@@ -78,64 +78,90 @@ sums
 sums2 <- sums |>
   mutate(kappa = paste0("log(\u03BA)=", kappa)) |>
   bind_rows(sums |>
-              filter(kappa == min(kappa)) |>
-              dplyr::select(-sum_proj, -sum_proj2) |>
-              rename(sum_proj = sum_orig,
-                     sum_proj2 = sum_orig2) |>
-              mutate(kappa = "Original")) |>
+    filter(kappa == min(kappa)) |>
+    dplyr::select(-sum_proj, -sum_proj2) |>
+    rename(
+      sum_proj = sum_orig,
+      sum_proj2 = sum_orig2
+    ) |>
+    mutate(kappa = "Original")) |>
   dplyr::select(-sum_orig, -sum_orig2) |>
   mutate() |>
   pivot_longer(-kappa, names_to = "var", values_to = "sum") |>
-  mutate(type = ifelse(var == "sum_proj", "point", "gmrf"),
-         sum_proj = paste0("Sum=", round(sum, digits = 2)))
+  mutate(
+    type = ifelse(var == "sum_proj", "point", "gmrf"),
+    sum_proj = paste0("Sum=", round(sum, digits = 2))
+  )
 
-scale_values <- function(x){(x-min(x))/(max(x)-min(x))}
+scale_values <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
 
 stuff2 <- stuff |>
   mutate(kappa = paste0("log(\u03BA)=", kappa)) |>
   bind_rows(stuff |>
-              filter(kappa == min(kappa)) |>
-              dplyr::select(-proj, -proj2) |>
-              rename(proj = orig,
-                     proj2 = orig2) |>
-              mutate(kappa = "Original")) |>
+    filter(kappa == min(kappa)) |>
+    dplyr::select(-proj, -proj2) |>
+    rename(
+      proj = orig,
+      proj2 = orig2
+    ) |>
+    mutate(kappa = "Original")) |>
   dplyr::select(-orig, -orig2) |>
   pivot_longer(cols = c("proj", "proj2")) |>
   mutate(type = ifelse(name %in% c("orig2", "proj2"), "gmrf", "point")) |>
-  mutate(value_sc = scale_values(value),
-         .by = c(name, kappa))
+  mutate(
+    value_sc = scale_values(value),
+    .by = c(name, kappa)
+  )
 
 p <- ggplot() +
-  facet_wrap(~factor(kappa, levels = c("Original", "log(κ)=5", "log(κ)=2.5", "log(κ)=0")),
-             ncol = 4) +
-  facet_wrap(~factor(kappa, levels = c("Original", "log(κ)=5", "log(κ)=2.5", "log(κ)=0")),
-             ncol = 4) +
+  facet_wrap(~ factor(kappa, levels = c("Original", "log(κ)=5", "log(κ)=2.5", "log(κ)=0")),
+    ncol = 4
+  ) +
+  facet_wrap(~ factor(kappa, levels = c("Original", "log(κ)=5", "log(κ)=2.5", "log(κ)=0")),
+    ncol = 4
+  ) +
   scale_fill_viridis(name = "Scaled\ncovariate", option = "G") +
   scale_color_viridis(option = "G") +
   theme_void() +
   guides(color = "none") +
-  theme(axis.title = element_blank(),
-        axis.text = element_blank())
+  theme(
+    axis.title = element_blank(),
+    axis.text = element_blank()
+  )
 
 t <- p +
-  geom_sf(data = stuff2 |> filter(type == "point"),
-          aes(fill = value_sc, color = value_sc)) +
-  geom_text(data = sums2 |> filter(type == "point"),
-            aes(x = -Inf, y = Inf, label = sum_proj),
-            hjust = -0.3, vjust = 2.5, color = "white", size = 2, fontface = "plain") +
+  geom_sf(
+    data = stuff2 |> filter(type == "point"),
+    aes(fill = value_sc, color = value_sc)
+  ) +
+  geom_text(
+    data = sums2 |> filter(type == "point"),
+    aes(x = -Inf, y = Inf, label = sum_proj),
+    hjust = -0.3, vjust = 2.5, color = "white", size = 2, fontface = "plain"
+  ) +
   labs(y = "Point") +
-  theme(axis.title.y = element_text(size = 8, angle = 90),
-        strip.text.x.top = element_text(size = 10, margin = unit(rep(0.08, 4), "cm")))
+  theme(
+    axis.title.y = element_text(size = 8, angle = 90),
+    strip.text.x.top = element_text(size = 10, margin = unit(rep(0.08, 4), "cm"))
+  )
 
 b <- p +
-  geom_sf(data = stuff2 |> filter(type == "gmrf"),
-          aes(fill = value_sc, color = value_sc)) +
-  geom_text(data = sums2 |> filter(type == "gmrf"),
-            aes(x = -Inf, y = Inf, label = sum_proj),
-            hjust = -0.3, vjust = 2.5, color = "white", size = 2, fontface = "plain") +
+  geom_sf(
+    data = stuff2 |> filter(type == "gmrf"),
+    aes(fill = value_sc, color = value_sc)
+  ) +
+  geom_text(
+    data = sums2 |> filter(type == "gmrf"),
+    aes(x = -Inf, y = Inf, label = sum_proj),
+    hjust = -0.3, vjust = 2.5, color = "white", size = 2, fontface = "plain"
+  ) +
   labs(y = "IID standard normal distributions") +
-  theme(axis.title.y = element_text(size = 8, angle = 90),
-        strip.text.x.top = element_blank())
+  theme(
+    axis.title.y = element_text(size = 8, angle = 90),
+    strip.text.x.top = element_blank()
+  )
 
 # b +
 #   scale_fill_viridis(name = "Scaled\ncovariate", option = "G") +
@@ -143,10 +169,12 @@ b <- p +
 
 (t / b) + plot_layout(guides = "collect") &
   coord_sf(xlim = c(0.05, 0.99), ylim = c(0.05, 0.99)) &
-  theme(legend.key.height = unit(0.4, "cm"),
-        legend.key.width = unit(0.3, "cm"),
-        legend.title = element_text(size = 8))
+  theme(
+    legend.key.height = unit(0.4, "cm"),
+    legend.key.width = unit(0.3, "cm"),
+    legend.title = element_text(size = 8)
+  )
 
 ggsave(paste0(here::here(), "/results/figures/theory_check.pdf"),
-       width = 18, height = 9, unit = "cm", device = cairo_pdf)
-
+  width = 18, height = 9, unit = "cm", device = cairo_pdf
+)
