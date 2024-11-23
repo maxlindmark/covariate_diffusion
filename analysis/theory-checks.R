17f55ec4f70edf775d03a8e8c5e4d77b666851f9
Sean Anderson <sean@seananderson.ca>
2024-11-22 16:03:56 -0800

    Update benchmark

--- analysis/theory-checks.R
+++ analysis/theory-checks.R
@@ -95,7 +95,7 @@ D2_b = solve(exp(ln_tau) * invM0 %*% (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g
 ###################
 #
 # Work out proportionality constant
-#  Visualize 
+#  Visualize
 #
 ###################
 
@@ -123,28 +123,40 @@ matplot( x=X, y=cbind(1/Y,1/Y2) )
 
 library(fmesher)
 library(Matrix)
-library(rbenchmark)
-
-run = function(n){
+library(ggplot2)
+run <- function(n) {
+  cat(n, "\n")
   # Simulate locations
-  loc = matrix( rnorm(n*2), ncol=2 )
+  loc <- matrix(rnorm(n * 2), ncol = 2)
   # Make SPDE objects
-  mesh = fm_mesh_2d( loc )
-  spde = fm_fem( mesh )
-  invM0 = spde$c0
-  diag(invM0) = 1/diag(spde$c0)
+  mesh <- fm_mesh_2d(loc)
+  spde <- fm_fem(mesh)
+  invM0 <- spde$c0
+  diag(invM0) <- 1 / diag(spde$c0)
   # Create inverse-D matrix
-  ln_kappa = log(5)
-  ln_tau = log( 1 / (1 + exp(2 * ln_kappa)) )
-  invD = exp(ln_tau) * invM0 %*% (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g1)
-  x = rnorm(mesh$n)
+  ln_kappa <- log(5)
+  ln_tau <- log(1 / (1 + exp(2 * ln_kappa)))
+  invD <- exp(ln_tau) * invM0 %*% (spde$c0 + exp(2 * ln_kappa) * spde$c0 + spde$g1)
+  x <- rnorm(mesh$n)
   # Do benchmark
-  out = benchmark( solve(invD,x), solve(invD) %*% x, columns=c("elapsed") )
-  return(out[,1])
+  f1 <- function() solve(invD, x)
+  f2 <- function() xx <- solve(invD) %*% x
+  bench::mark(f1(), f2(), check = FALSE)
 }
+size <- c(10, 100, 200, 500, 1000, 2000)
+b <- lapply(size, run)
+times <- purrr::map_dfr(seq_along(b), \(i)
+  data.frame(
+    approach = c("Using inverse diffusion matrix directly", "Computing diffusion matrix"),
+    time = as.numeric(b[[i]]$median),
+    size = size[i]
+  )
+)
+g <- ggplot(times, aes(size, time, colour = approach)) + geom_line() +
+  labs(x = "N", y = "Seconds per calculation", colour = "Approach") +
+  scale_colour_brewer(palette = "Set2") + ggsidekick::theme_sleek() +
+  scale_y_continuous(lim = c(-0.01, NA), expand = expansion(mult = c(0, 0.05)))
+g
 
-size = ceiling(10 ^ seq(0,3,length=10))
-bench = sapply( size, FUN=run )
-
-matplot( x=size, y=t(bench), type="l", lty="solid" )
+g + scale_y_log10()
 
