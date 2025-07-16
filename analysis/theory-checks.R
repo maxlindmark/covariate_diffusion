3c7694c4dbeab6385d4d6e3d46026cc624859b48
Jim Thorson <James.T.Thorson@gmail.com>
2025-07-16 07:10:54 -0700

    Update theory-checks.R

--- analysis/theory-checks.R
+++ analysis/theory-checks.R
@@ -9,11 +9,13 @@ library(sf)
 set.seed(123)
 
 #
-#loc = matrix( runif(1000), ncol=2 )
-#loc = as.matrix( expand.grid(seq(0,1,len=20),seq(0,1,len=20)) )
 geoscale = 1  # < 0.01 leads to weird behavior in fm_mesh_2d
+
+# simulate locations from Poisson or Poisson-disk
 #loc = poisson2disk( n=1000, a = geoscale, b = geoscale )
 loc = matrix( geoscale * runif(2*10), ncol = 2 )
+
+# Make mesh and FEM matrices
 boundary = fm_segm( geoscale * rbind(c(0, 0), c(1, 0), c(1, 1), c(0, 1)),
                     is.bnd = TRUE)
 mesh = fm_mesh_2d( loc,
@@ -25,7 +27,6 @@ invM0 = spde$c0
 diag(invM0) = 1/diag(spde$c0)
 
 #
-#sf_mesh = st_union(fm_as_sfc(mesh))
 sf_grid = st_make_grid( st_multipoint(geoscale * cbind(c(0,0,1,1,0),c(0,1,1,0,0))), n=100 )
 A_gs = fm_evaluator( mesh, loc=st_coordinates(st_centroid(sf_grid)) )$proj$A
 
@@ -49,13 +50,6 @@ ln_kappa = log(1) - log(geoscale)
 invD = Diagonal(n=mesh$n) + exp(-2 * ln_kappa) * invM0 %*% spde$g1
 D = solve(invD)
 
-#
-#invDt = exp(ln_tau) * (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g1) %*% invM0
-
-# Sanity checks
-#f = function(kappa, tau) 1 / (1 + kappa^2) / tau
-#D = solve(invD)
-
 # for any density vector v, sum( A_gs * v ) should equal sum( A_gs * D * x )
 # so colSums( A_gs * I_ss ) should equal colSums( A_gs * D * I_ss )
 plot( colSums( A_gs ),  colSums( A_gs %*% D ) )
@@ -66,7 +60,7 @@ vec1 = rep(0,mesh$n)
 vec1[which_mid] = 1
 d0_g = A_gs %*% vec1
 d1_g = A_gs %*% solve(invD,vec1)
-# Cmopare the two
+# Compare the two
 t(rep(1,length(sf_grid))) %*% d0_g
 t(rep(1,length(sf_grid))) %*% d1_g
 
@@ -77,7 +71,6 @@ dist2_g = rowSums((st_coordinates(st_centroid(sf_grid)) - outer(rep(1,length(sf_
 (RMSD = sqrt(weighted.mean( dist2_g, w=d1_g)) ) / geoscale
 
 # plot diffusion from a specified point
-# plotting code
 stuff = st_sf( sf_grid,
                "orig"=as.numeric(A_gs%*%vec1),
                "proj"=as.numeric(A_gs%*%solve(invD,vec1)) )   # , log(as.numeric(vec2b)))
@@ -85,18 +78,13 @@ plot( stuff, cex=2, pch=19, border=NA )
 sum( stuff$orig )
 sum( stuff$proj )
 
-#plot( st_sf( sf_grid, as.numeric(A_gs %*% colSums(A_gs)) ), border = NA )
-#dev.new()
-#plot( st_sf( sf_grid, as.numeric(A_gs %*% colSums(A_gs %*% D)) ), border = NA )
-
-# Using IID normal deviates
+# Check with IID normal deviates
 vec1 = rnorm(mesh$n)
-# Cmopare the two
+# Compare the two
 t(rep(1,length(sf_grid))) %*% A_gs %*% vec1
 t(rep(1,length(sf_grid))) %*% A_gs %*% solve(invD,vec1)
 
 # plot diffusion from a specified point
-# plotting code
 stuff = st_sf( sf_grid,
                "orig"=as.numeric(A_gs%*%vec1),
                "proj"=as.numeric(A_gs%*%solve(invD,vec1)) )   # , log(as.numeric(vec2b)))
@@ -104,48 +92,6 @@ plot( stuff, cex=2, pch=19, border=NA )
 sum( stuff$orig )
 sum( stuff$proj )
 
-###################
-#
-# Other sanity checks
-#
-###################
-
-
-#
-ln_kappa = log(5)
-ln_tau = log( 1 / (1 + exp(2 * ln_kappa)) )
-D = solve(exp(ln_tau) * invM0 %*% (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g1))
-D2 = D %*% D
-
-#
-ln_kappa = log(5) - log(2)
-ln_tau = log( 1 / (1 + exp(2 * ln_kappa)) )
-D2_b = solve(exp(ln_tau) * invM0 %*% (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g1))
-
-
-###################
-#
-# Work out proportionality constant
-#  Visualize
-#
-###################
-
-f = function( kappa, tau ){
-  ln_kappa = log(kappa)
-  ln_tau = log(tau)
-  invD = exp(ln_tau) * invM0 %*% (spde$c0 + exp(2*ln_kappa)*spde$c0 + spde$g1)
-  vec = solve(invD, rep(1,mesh$n))
-  mean(vec)
-}
-X = seq(0,10,length=21)
-Y = sapply( X, FUN=\(x)f(x,1) )
-
-# Formula I worked out by pattern matching
-f2 = \(kappa, tau) 1 / (1 + kappa^2) / tau
-Y2 = sapply( X, FUN=\(x)f2(x,1) )
-
-# Compare the two
-matplot( x=X, y=cbind(1/Y,1/Y2) )
 
 
 ################
